9921c1fd0c93d158aee0f8eec1699c55a78f4d28
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-06-06 09:54:33 +0000

    x

--- notes/note2_camouflage/main.js
+++ notes/note2_camouflage/main.js
@@ -113,10 +113,10 @@ class App{
         this.gl.uniform2f(this.uniformLocationMap["u_resolution"], this.canvas.width, this.canvas.height)
         this.gl.uniform1f(this.uniformLocationMap["u_pixelSize"], this.u_pixelSize)
         this.gl.uniform3f(this.uniformLocationMap["u_bgColor"], 0.271, 0.482, 0.616)
-        this.gl.uniform3fv(this.uniformLocationMap["u_colors"],new Float32Array([0.659, 0.855, 0.863, 0.114, 0.208, 0.341, 0.945, 0.98, 0.933]));
-        this.gl.uniform2fv(this.uniformLocationMap["u_minMaxRads"], new Float32Array([-2,2, -4,4, -4,4]))
-        this.gl.uniform2fv(this.uniformLocationMap["u_centers"], new Float32Array(0))
-        this.gl.uniform1fv(this.uniformLocationMap["u_sizes"], new Float32Array(0))
+        this.gl.uniform3fv(this.uniformLocationMap["u_colors"],new Float32Array([ 0.114, 0.208, 0.341, 0.945, 0.98, 0.933, 0.659, 0.855, 0.863]));
+        this.gl.uniform2fv(this.uniformLocationMap["u_minMaxRads"], new Float32Array([ -6,4, -6,4, -2,2]))
+        // this.gl.uniform2fv(this.uniformLocationMap["u_centers"], [])
+        // this.gl.uniform1fv(this.uniformLocationMap["u_sizes"], [])
         this.gl.uniform1i(this.uniformLocationMap["u_arrayLength"], 0)
     }
     setAttribute(){
@@ -178,16 +178,17 @@ class App{
 
         this.shrinkingCircle = this.shrinkingCircle.filter(e=>e.size > 15);
 
-        const centers = this.shrinkingCircle.flatMap(c=>[c.centerX,c.centerY])
-        const sizes = this.shrinkingCircle.map(c=>c.size);
+        let centers = this.shrinkingCircle.flatMap(c=>[c.centerX,c.centerY])
+        let sizes = this.shrinkingCircle.map(c=>c.size);
+        console.log(centers)
 
         if(this.growingCircle){
             centers.push(this.growingCircle.centerX, this.growingCircle.centerY)
             sizes.push(this.growingCircle.size)
         }
         
-        this.gl.uniform2fv(this.uniformLocationMap["u_centers"], new Float32Array(centers));
-        this.gl.uniform1fv(this.uniformLocationMap["u_sizes"], new Float32Array(sizes));
+        // this.gl.uniform2fv(this.uniformLocationMap["u_centers"], centers);
+        // this.gl.uniform1fv(this.uniformLocationMap["u_sizes"], sizes);
         this.gl.uniform1i(this.uniformLocationMap["u_arrayLength"], sizes.length);
 
     }
