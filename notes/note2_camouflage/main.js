240840c4214cfbaf328966e6efa4ddbe175c041b
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-07-08 02:43:05 +0000

    x

--- notes/note2_camouflage/main.js
+++ notes/note2_camouflage/main.js
@@ -245,7 +245,7 @@ class App{
         if(this.growingCircle){
             this.growingCircle.size += this.deltaTime * 40;
         }
-        
+        console.log(this.shrinkingCircle)
         for(let circle of this.shrinkingCircle){
             
             circle.size -= this.deltaTime * 40;
